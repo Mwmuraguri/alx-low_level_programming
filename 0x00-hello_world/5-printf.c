af3850324abbae4c83e7309cd713618d3251add6
Mwmuraguri <maryannewanjiku87@gmail.com>
2023-03-16 14:23:13 -0700

    A C program that prints with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with printf function
- * Return: 0 (success)
+ * main - A C program that prints a line with printf function
+ * Return: 0 (Good)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 }
