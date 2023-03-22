8dcf5231727e46da1481e8c59be2c123b0fda9ba
Mwmuraguri <maryannewanjiku87@gmail.com>
2023-03-22 12:59:29 -0700

    function that prints the sign of a number

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,39 +1,33 @@
 #include "main.h"
-/**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of times table
- */
-void print_times_table(int n)
-{
-	int num, mult, prod;
 
-	if (n >= 0 && n <= 15)
-	{
-	for (num = 0; num <= n; num++)
-	{
-	_putchar('0');
-	for (mult = 1; mult <= n; mult++)
-	{
-	_putchar(',');
-	_putchar(' ');
-	prod = num * mult;
-	if (prod <= 99)
-	_putchar(' ');
-	if (prod <= 9)
-	_putchar(' ');
-	if (prod >= 100)
-	{
-	_putchar((prod / 100) + '0');
-	_putchar(((prod / 10)) % 10 + '0');
-	}
-	else if (prod <= 99 && prod >= 10)
-	{
-	_putchar((prod / 10) + '0');
-	}
-	_putchar((prod % 10) + '0');
-	}
-	_putchar('\n');
-	}
-	}
+
+/**
+
+ * print_sign - prints the sign of a number
+
+ * @n: the int to check
+
+ * Return: 1 and prints + if n is greater than zero
+
+ * 0 and prints 0 if n is zero
+
+ * -1 and prints - if n is less than zero
+
+ */
+
+int print_sign(int n)
+
+{
+
+        if (n > 0)
+
+        {
+
+                _putchar('+');
+
+                return (1);
+
+        } else if (n == 0)
+
 }
 
