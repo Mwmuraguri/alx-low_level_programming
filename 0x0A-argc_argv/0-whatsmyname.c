7ed0b8b15cac7cc401d11dd000e272ba9318c980
Mwmuraguri <maryannewanjiku87@gmail.com>
2023-04-10 03:07:31 -0700

    Prints the name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name followed by a new line
+ * print a new name without compiling it again
+ * does not remove the path before the name of the program
+ * @argc: no. of arguments
+ * @argv: array of arguments
+ *
+ * Return: always (0)
+ */
+int main(char *argv[], int  argc)
+{
+	printf("%d\n", *argv[0]);
+	return (0);
+}
