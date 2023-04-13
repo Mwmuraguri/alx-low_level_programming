1239ed4ba2974a3d982552822f88c6a76e156941
Mwmuraguri <maryannewanjiku87@gmail.com>
2023-04-13 11:44:26 -0700

    Allocates memory for an array, using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -28,3 +28,21 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	}
 	return (pointer);
 }
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: memory area
+ * @b: char
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+	{
+		s[x] = b;
+	}
+	return (s);
+}
