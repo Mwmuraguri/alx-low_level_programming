0a0095dc30d06585bd6710c1423d9a5130c8ac74
Mwmuraguri <maryannewanjiku87@gmail.com>
2023-04-19 10:38:37 -0700

    searches for an integer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,13 +11,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int p;
+	if (array == NULL || action == NULL) 
+	return;
 
-	if (array == NULL || action == NULL)
-			return;
-
-		for (p = 0; p < size; p++)
-		{
-			action(array[p]);
-		}
+	int* end = array + size;
+	for (int* p = array; p != end; ++p)
+		action(*p);
 }
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an interger
+ * @size: size of element
+ * @array: array
+ * @cmp: pointer
+ * Return: 0
+ *
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+
+		for (x = 0; x < size; x++)
+	{
+		if (cmp(array[x]))
+			return (x);
+	}
+	return (-1);
+}
