fac1cd833f3b4aa5ec508b181079207217b5eb71
Mwmuraguri <maryannewanjiku87@gmail.com>
2023-04-19 11:13:55 -0700

    searches for an integer

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,8 @@
 #ifndef FUCTION_POINTERS_H
 #define FUCTION_POINTERS_H
 
-#include <stdlib.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
