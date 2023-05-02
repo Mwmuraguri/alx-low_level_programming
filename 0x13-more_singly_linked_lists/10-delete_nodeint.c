266c2ff0a0683e46351f1ccfa55401474067615e
Mwmuraguri <maryannewanjiku87@gmail.com>
2023-05-02 02:34:54 -0700

    Deletes the node at index index of a listint_t linked list.

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,17 +8,16 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *temp = NULL;
 
-	if (!head)
+	if (head == NULL)
 		return;
 
-	while (*head)
+	while (*head != NULL)
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
-	}
 
-	*head = NULL;
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
 }
